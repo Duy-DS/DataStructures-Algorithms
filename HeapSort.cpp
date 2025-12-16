8141f8ee8722049f637ab1385588588d1578bce1
DY-DS <199239626+Duy-DS@users.noreply.github.com>
2025-12-16 08:38:58 +0700

    Enhance comments in HeapSort.cpp for clarity
    Added detailed comments explaining the heap sort algorithm and its implementation.

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -3,20 +3,23 @@
 using namespace std;
 
 void heapSort(int a[], int n) {
-	// xay dung max-heap
+	// xay dung max-heap -> phần tử lớn nhất luôn nằm ở gốc (a[0])
 	for (int start = n / 2 - 1; start >= 0; start--) {
-		int root = start;
+		// Vì sao bắt đầu từ n/2 - 1?; Các phần tử từ n/2 → n-1 là lá -> Lá đã là heap -> Ta chỉ cần heapify các nút cha
+		int root = start; //root là nút đang xét, ta sẽ đẩu nó xuống đúng vị trí trong heap
 
-		while (true) {
-			int left = 2 * root + 1;
-			int right = 2 * root + 2;
-			int largest = root;
+		while (true) {//Lặp cho đến khi root đã đúng vị trí
+			int left = 2 * root + 1;//con trái
+			int right = 2 * root + 2;//con phải
+			int largest = root;//Giả sử root là lớn nhất
 
+			//Kiểm tra con trái có tồn tại không, nếu con trái lớn hơn root -> chọn con trái
 			if (left < n && a[left]>a[largest]) largest = left;
-
+			
+			//Kiểm tra con phải có tồn tại không, nếu con phải lớn hơn root -> chọn con phải
 			if (right < n && a[right]>a[largest]) largest = right;
 
-			if (largest != root) {
+			if (largest != root) {//Nếu root KHÔNG lớn nhất → đổi chỗ
 				swap(a[root], a[largest]);
 				root = largest;
 			}
@@ -24,19 +27,20 @@ void heapSort(int a[], int n) {
 		}
 	}
 
+	//Sắp xếp bằng cách lấy max
 	for (int end = n - 1; end > 0; end--) {
-		swap(a[0], a[end]);//dua max ve cuoi
+		swap(a[0], a[end]);//lấy max a(a[0]) dua max ve cuoi
 
-		int root = 0;
+		int root = 0;//khôi phục heap cho phần còn lại; Heap lại bắt đầu từ gốc
 
-		while (true) {
+		while (true) {//Heapify lại (giống phần trên)
 			int left = 2 * root + 1;
 			int right = 2 * root + 2;
 			int largest = root;
 
-			if (left<end && a[left]>a[largest]) largest = left;
+			if (left<end && a[left]>a[largest]) largest = left; //left < end: Không xét phần đã sắp xếp
 
-			if (right <end && a[right]>a[largest]) largest = right;
+			if (right <end && a[right]>a[largest]) largest = right;//left < end: Không xét phần đã sắp xếp
 
 			if (largest != root) {
 				swap(a[root], a[largest]);
@@ -46,3 +50,4 @@ void heapSort(int a[], int n) {
 		}
 	}
 }
+
